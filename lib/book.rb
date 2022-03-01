31e3b46a8e33703238fae008ad7f4f233ebaa2ff
Zach <zawood11@gmail.com>
2022-02-28 23:28:38 -0500

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :genre, :page_count
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
